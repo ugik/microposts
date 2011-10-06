af8abc9f9845cc0390af3eced879dd9577cb7a93
GK <georgek@gmail.com>
2011-10-06 16:36:46 -0400

    about page cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Return a title on a per-page basis.
   def title
-    base_title = "Keas HR Console"
+    base_title = "Keas Console"
     if @title.nil?
       base_title
     else
@@ -12,6 +12,10 @@ module ApplicationHelper
   
   def logo
     image_tag("logo.png", :alt => "Console", :class => "round", :width => "30")
+  end  
+  
+  def pop
+    image_tag("PowerOfPlay.jpg", :alt => "Console", :class => "round", :width => "250")
   end  
   
 end
